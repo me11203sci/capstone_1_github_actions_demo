316161ba7b34db5383f25ed50fabc089ef7ce4c0
Melesio Albavera <ma6hv@umsystem.edu>
2025-12-05 03:52:51 +0000

    Added dunder

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-root = Path(file).resolve().parents[1]
+root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 from app import add
